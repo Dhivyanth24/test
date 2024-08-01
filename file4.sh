2ff88b8f9e7c4566bf80e884624d7609d51f7328
Dhivyanth24 <dhivyanth@trustt.com>
2024-08-01 14:58:42 +0530

    file4 created

---
+++ file4.sh
@@ -0,0 +1,10 @@
+echo "version control system"
+pwd
+date
+echo "keep track on changes"
+uptime
+java --version
+echo "collaborate on code"
+echo "bye"
+date | cut --delimiter " " --fields=1 | echo good
+echo "!"
